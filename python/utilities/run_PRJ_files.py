db0faa0af49a9070d1e4e664d91d9fbef6af0f6c
Von P. Walden <v.walden@wsu.edu>
2018-11-27 12:44:30 -0800

    Minor change in formatting...

--- python/utilities/run_PRJ_files.py
+++ python/utilities/run_PRJ_files.py
@@ -12,13 +12,12 @@ from subprocess import call
 site_data = pd.read_csv('/mnt/data/lima/iaq/cmaq/sites.csv')
 
 for row, city in site_data.iterrows():
-    if city['City_Name'] == " ": continue
-    
-    for rcp in ["4.5", "8.5"]:
-        rcpdir   = '/mnt/data/lima/iaq/cmaq/prj' + rcp + '/' + city['City_Name'] + '/'
-        os.chdir(rcpdir)
-        prjFiles = glob(rcpdir+'*.prj')
-        prjFiles.sort()
-        
-        for prjFile in prjFiles[0:10]:
-            call(['/mnt/data/lima/contam/contam-x-3.2-gcc.exe',prjFile])
+    if (city['City_Name']=='Dallas') or (city['City_Name']=='Washington'):
+        for rcp in ["4.5", "8.5"]:
+            rcpdir   = '/mnt/data/lima/iaq/cmaq/prj' + rcp + '/' + city['City_Name'] + '/'
+            os.chdir(rcpdir)
+            prjFiles = glob(rcpdir+'*.prj')
+            prjFiles.sort()
+            
+            for prjFile in prjFiles[0:10]:
+                call(['/mnt/data/lima/contam/contam-x-3.2-gcc.exe',prjFile])
