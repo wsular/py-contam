f53415f39681a902048795ea9f7e374fcb143900
Von Walden <vonw@petb227a.paccar.wsu.edu>
2018-12-14 22:08:26 -0800

    Updates to files as I consolidate software for running contam.

--- python/maca/createMACAweatherFiles.py
+++ python/maca/createMACAweatherFiles.py
@@ -25,8 +25,10 @@ else:
 rcps   = ['4.5', '8.5']
 #rcps   = ['8.5']
 #years  = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+years  = [2048, 2052]   # leap years
 #years  = [2045, 2046, 2047, 2048, 2049, 2050, 2051, 2052, 2053, 2054]
-years  = [2090, 2091, 2092, 2093, 2094, 2095, 2096, 2097, 2098]
+#years  = [2090, 2091, 2092, 2093, 2094, 2095, 2096, 2097, 2098]
+#years  = [2092, 2096]   # leap years
 
 cities = pd.read_csv(in_d + 'iaq_cities.csv')
 models = ['CCSM4','CNRM-CM5','GFDL-ESM2M','HadGEM2-ES365','IPSL-CM5A-LR','MIROC-ESM','MIROC5']
--- python/maca/removeFeb29_LeapYears.py
+++ python/maca/removeFeb29_LeapYears.py
@@ -8,6 +8,7 @@ d = '/mnt/data/lima/iaq/maca'
 files = glob(d+'*.csv')
 
 for f in files:
+   print(f)
    df = pd.read_csv(f, index_col='time', parse_dates=True)
    dfn = df[~((df.index.month == 2) & (df.index.day == 29))]
    dfn.to_csv(f)
