cf8d133d3e293a38a5ba76fab53a04dc2918c0bf
Von P. Walden <v.walden@wsu.edu>
2019-02-22 10:31:00 -0800

    Updates to code to extract years 1995-2006 and 2086-2089 from original MACA files from Abatzoglou at U. Idaho.

--- python/maca/createMACAcsvFiles.py
+++ python/maca/createMACAcsvFiles.py
@@ -11,7 +11,7 @@ import pandas      as     pd
 from   maca_aeolus import maca
 
 # Base directory for IAQ data.
-d          = '/Volumes/vonw/data/iaq/maca/'
+d          = '/Volumes/sdata/data/iaq/maca/'
 # Open the file containing the list of cities.
 cities        = pd.read_csv('/Users/vonw/work/software/iaq/py-contam/python/iaq_cities.csv')
 
@@ -20,6 +20,9 @@ fvar          = ['pr','tasmin','tasmax','uas','vas','huss']
 # NEED TO DOWNLOAD HUSS FILE FOR NOR-ESM1-M MODEL.
 models        = ['CCSM4','CNRM-CM5','GFDL-ESM2M','HadGEM2-ES365','IPSL-CM5A-LR','MIROC-ESM','MIROC5']
 # Beginning and ending dates for various decades.
+#1990s
+#beginningDate = '1995-12-31'
+#endingDate    = '2006-01-02'
 #2010s
 #beginningDate = '2009-12-31'
 #endingDate    = '2020-01-02'
@@ -30,8 +33,10 @@ models        = ['CCSM4','CNRM-CM5','GFDL-ESM2M','HadGEM2-ES365','IPSL-CM5A-LR',
 #beginningDate = '2044-12-31'
 #endingDate    = '2056-01-02'
 #2090s
-beginningDate = '2089-12-31'
-endingDate    = '2099-01-02'
+beginningDate = '2085-12-31'
+endingDate    = '2096-01-02'
+#beginningDate = '2089-12-31'
+#endingDate    = '2099-01-02'
 
 
 for city in cities.city:
