8c1175b5f77e35e9761b1a21f8d9ab657c635720
Von P. Walden <v.walden@wsu.edu>
2019-08-25 13:15:41 -0700

    Completed version that now generates wth and ctm files.

--- python/houses/createHouseContaminantFiles.py
+++ python/houses/createHouseContaminantFiles.py
@@ -16,6 +16,7 @@ ds = glob('/Users/vonw/data/iaq/houses/outdoors/' + '*.xlsx')
 ds.sort()
 ds = [d.split('.')[0] + '/' for d in ds]
 
+#............................... Weather .....................................
 # ....Read weather data
 wth = pd.read_csv('/Users/vonw/data/iaq/houses/weatherFiles/H005_summer.wth', 
                   sep='\t', 
@@ -32,11 +33,45 @@ wth['minute']   = times[1]
 wth['second']   = times[2]
 wth.index       = pd.to_datetime(wth[['year', 'month', 'day', 'hour', 'minute', 'second']])
 
+# ....Convert sea-level pressures to station pressure using:
+#         https://www.weather.gov/media/epz/wxcalc/stationPressure.pdf
+hm  = 117.         # Richland, WA: House 5
+#hm  = 716.9        # Pullman, WA: Houses 1, 2, 3, 4, 6, 7, 8
+#hm  = 601.1        # Colfax, WA: Houses 9, 10
+wth['Pb']  = wth['Pb'] * ( ( 288. - 0.0065*hm) / 288. ) ** 5.2561
+# ....Calculate air density
+wth['density'] = wth['Pb'] / (287. * wth['Ta'])   # 287 is the gas constant for air in J/(kg K).
+ 
+#............................ Contaminants ...................................
 # ....Read contaminant data
 #for d in ds:
 d  = ds[6]      # House 5
-ctm, units = contam.readHouseContaminantData(d)
+#rack, pm25, ptrms = contam.readHouseContaminantData(d)
+rack, pm25, ptrms = readHouseContaminantData(d)
+pm25 = pm25.drop(columns=['Unnamed: 2_level_0', 'Unnamed: 3_level_0', 'Unnamed: 4_level_0'])
 
-time_min = max([min(wth.index), min(ctm.loc['rack'].index), min(ctm.loc['pm25'].index), min(ctm.loc['ptrms'].index)])
-time_max = min([max(wth.index), max(ctm.loc['rack'].index), max(ctm.loc['pm25'].index), max(ctm.loc['ptrms'].index)])
+time_min = max([min(wth.index), min(rack.index), min(pm25.index), min(ptrms.index)])
+time_max = min([max(wth.index), max(rack.index), max(pm25.index), max(ptrms.index)])
 
+wth   = wth[time_min:time_max]
+rack  = rack[time_min:time_max].resample('30T').mean()
+pm25  = pm25[time_min:time_max].resample('30T').mean()
+ptrms = ptrms[time_min:time_max].resample('30T').mean()
+ctm   = pd.concat([rack[1:], pm25[1:], ptrms[1:]], axis=1)
+
+for v, u in ctm.columns:
+    if   u == 'ppm':
+        ctm[v] = ctm[v] / 1e6
+    elif u == 'ppbv':
+        ctm[v] = ctm[v] / 1e9
+    elif u == 'ug/m3':
+        ctm[v] = ctm[v].values / (wth['density'].values * 1e9)
+    else:
+        pass
+
+# ....Write weather and contaminant files to contam simulations.
+#     Weather file
+contam.writeContamWeatherFile(d + d.split('/')[-2] + '.wth', wth)
+#     Contaminant file
+ctm.columns = ctm.columns.droplevel(1) # drop level 1 of columns so that df works with write function.
+contam.writeContamSpeciesFile(d + d.split('/')[-2] + '.ctm', ctm)
