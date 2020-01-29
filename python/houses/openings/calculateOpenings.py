a74b143a84a935e86c2f76b06576cc664577d233
Von P. Walden <v.walden@wsu.edu>
2020-01-29 12:18:36 -0800

    Shorten variable names to be less than 15 characters

--- python/houses/openings/calculateOpenings.py
+++ python/houses/openings/calculateOpenings.py
@@ -13,21 +13,22 @@ import pandas as pd
 import numpy  as np
 
 # ---- NECESSARY USER INPUT ----
-d = '/Users/vonw/data/iaq/houses/SmartHome/YiBo_UTC_WD_CleanedJitter/Atmo10W/'
-validSensors = ['DoorB', 'DoorC', 'DoorD', 'MainDoor']
+d = '/Users/vonw/data/iaq/houses/SmartHome/YiBo_UTC_WD_CleanedJitter/Atmo5S/'
+# validSensors = ['DoorB', 'DoorC', 'DoorD', 'MainDoor']
 # ------------------------------
 
 fns = glob(d + '*.txt')
 fna = glob(d + 'atmo*.csv')[0]
 
-openings = [fn.split('/')[-1].split('.')[0] for fn in fns]
 #%%
 # Read all data files for house openings
+openings = []
 houses = pd.read_csv('/Users/vonw/work/software/iaq/py-contam/python/houses/openings/houseDates.csv', parse_dates=['StartDate','EndDate'])
 data   = {}
 for fn in fns:
     print(fn)
     opening = fn.split('/')[-1].split('.')[0]
+    openings.append(opening)
     df = pd.read_csv(fn, sep='\t', names=['time', 'opening', 'state'], parse_dates=[0])
     # ....Sets up the time index
     df.index = df.time
@@ -37,6 +38,12 @@ for fn in fns:
     df['state'].loc[df['state']=='CLOSE'] = 0
     df['state'].loc[df['state']=='OPEN']  = 1
     # ....Upsamples the data to 1 second intervals, then downsamples to 30 minutes.
+    opening = opening.replace('Temperature','')
+    opening = opening.replace('Bathroom','Bath')
+    opening = opening.replace('Bedroom','Bed')
+    opening = opening.replace('Kitchen','Kit')
+    opening = opening.replace('Office','Off')
+    opening = opening.replace('LivingRoom','Liv')
     data[opening] = df.resample('1S').ffill().resample('30min').mean()
     # data[opening].to_csv(d+opening+'.csv')
 
@@ -47,12 +54,18 @@ atmo = pd.read_csv(fna, parse_dates=[0, 1], names=['time', 'location', 'Temperat
 atmo.index = atmo.time
 sensors = atmo.location.unique()
 for sensor in sensors:
-    if(sensor in validSensors):
-        data['T_' + sensor.rstrip()] = (atmo[atmo.location == sensor].Temperature+273.15).resample('1S').ffill().resample('30min').mean()
+    if(sensor.strip() in openings):
+        sensorName = sensor.replace('Temperature','')
+        sensorName = sensorName.replace('Bathroom','Bath')
+        sensorName = sensorName.replace('Bedroom','Bed')
+        sensorName = sensorName.replace('Kitchen','Kit')
+        sensorName = sensorName.replace('Office','Off')
+        sensorName = sensorName.replace('LivingRoom','Liv')
+        data['T_' + sensorName.rstrip()] = (atmo[atmo.location == sensor].Temperature+273.15).resample('1S').ffill().resample('30min').mean()
 
 #%%
 # Determine the time scale
-minTime = np.array([data[key].index [0] for key in data]).max()
+minTime = np.array([data[key].index[0] for key in data]).max()
 minDate = minTime.date()
 maxTime = np.array([data[key].index[-1] for key in data]).min()
 maxDate = maxTime.date() + pd.Timedelta('1d')   # Add day to get ALL values of the last day.
